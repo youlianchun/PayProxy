bf7c97b6c6e49ccd5d0e1601bf91893d54cd13da
YLCHUN <ylchun@YLCHUN.lan>
2018-04-03 22:01:18 +0800

    no message

--- PayProxy/PayProxy/PayProxy.m
+++ PayProxy/PayProxy/PayProxy.m
@@ -10,13 +10,12 @@
 #import <AlipaySDK/AlipaySDK.h>
 #import "WXApi.h"
 
-static NSString * const kproxy = @"proxy";
 static NSString * const kwxPay = @"wxPay";
 static NSString * const kaliPay = @"aliPay";
 
 @interface PayProxy()<WXApiDelegate>
 {
-    BOOL _isPay;
+    NSString *_payType;
     PayResult _result;
     NSString *_wxAppkey;
     NSString *_aliAppkey;
@@ -35,9 +34,9 @@ static NSString * const kaliPay = @"aliPay";
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    if (_isPay)
+    if (_payType)
     {
-        [self logErr:kproxy code:-99 msg:@"用户放弃支付：其他途径返回app。"];
+        [self logErr:-99 msg:@"用户放弃支付：其他途径返回app。"];
         [self callPayResult:NO];
     }
 }
@@ -56,6 +55,7 @@ static NSString * const kaliPay = @"aliPay";
 -(void)wxPay:(NSDictionary*)signData res:(PayResult)res
 {
     _result = res;
+    _payType = kwxPay;
     PayReq *request = [[PayReq alloc] init];
     request.partnerId = signData[@"partnerid"];
     request.prepayId = signData[@"prepayid"];
@@ -63,36 +63,30 @@ static NSString * const kaliPay = @"aliPay";
     request.nonceStr = signData[@"noncestr"];
     request.timeStamp = [signData[@"timestamp"] unsignedIntValue];
     request.sign = signData[@"sign"];
-    _isPay = [WXApi sendReq:request];
-    if (!_isPay)
+    if (![WXApi sendReq:request])
     {
-        [self logErr:kwxPay code:WXErrCodeCommon msg:@"原因：签名数据错误。"];
+        [self logErr:WXErrCodeCommon msg:@"原因：签名数据错误。"];
         [self callPayResult:NO];
     }
 }
 
 -(void)aliPay:(NSString*)signData res:(PayResult)res
 {
-    _isPay = YES;
     _result = res;
+    _payType = kaliPay;
     [[AlipaySDK defaultService] payOrder:signData fromScheme:_aliAppkey callback:[self aliPayCallback]];
 }
 
 -(BOOL)handleOpenURL:(NSURL *) url
 {
-    BOOL isPay = _isPay;
-    _isPay = NO;
-    if (isPay) {
-        if (![self alipayHandleOpenURL:url]) {
-            [self wxpayHandleOpenURL:url];
-        }
-    }
-    return isPay;
+    if ([self alipayHandleOpenURL:url]) return YES;
+    if ([self wxpayHandleOpenURL:url]) return YES;
+    return NO;
 }
 
 -(BOOL)wxpayHandleOpenURL:(NSURL *) url
 {
-    if ([url.scheme isEqualToString:_wxAppkey]) {
+    if (_payType == kwxPay && [url.scheme isEqualToString:_wxAppkey]) {
         return [WXApi handleOpenURL:url delegate:self];
     }
     return NO;
@@ -103,7 +97,7 @@ static NSString * const kaliPay = @"aliPay";
     if([resp isKindOfClass:[PayResp class]]) {
         BOOL success = resp.errCode == WXSuccess;
         if (!success) {
-            [self logErr:kwxPay code:resp.errCode msg:resp.errStr];
+            [self logErr:resp.errCode msg:resp.errStr];
         }
         [self callPayResult:success];
     }
@@ -111,7 +105,7 @@ static NSString * const kaliPay = @"aliPay";
 
 -(BOOL)alipayHandleOpenURL:(NSURL *) url
 {
-    if ([url.scheme isEqualToString:_aliAppkey]) {
+    if (_payType == kaliPay && [url.scheme isEqualToString:_aliAppkey]) {
         [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:[self aliPayCallback]];
         return YES;
     }
@@ -127,7 +121,7 @@ static NSString * const kaliPay = @"aliPay";
             int code = [resultDic[@"resultStatus"] intValue];
             BOOL success = code == 9000;
             if (!success) {
-                [self logErr:kaliPay code:code msg:resultDic[@"result"]];
+                [self logErr:code msg:resultDic[@"result"]];
             }
             [self callPayResult:success];
         };
@@ -137,7 +131,7 @@ static NSString * const kaliPay = @"aliPay";
 
 -(void)callPayResult:(BOOL) success
 {
-    _isPay = NO;
+    _payType = nil;
     if (_result)
     {
         PayResult result = _result;
@@ -146,9 +140,9 @@ static NSString * const kaliPay = @"aliPay";
     }
 }
 
--(void)logErr:(NSString*)type code:(int)code msg:(NSString*)msg
+-(void)logErr:(int)code msg:(NSString*)msg
 {
-    NSLog(@"PayProxyError: %@, code: %d, message: %@", type, code, msg);
+    NSLog(@"PayProxyError: %@, code: %d, message: %@", _payType, code, msg);
 }
 
 @end
